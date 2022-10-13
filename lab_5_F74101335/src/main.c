0efd4ecae9302e3af9168d56ae54689c71cf080a
elsonkong0212 <elsonkong0212@gmail.com>
2022-10-14 01:34:26 +0800

    Lab-5

---
+++ lab_5_F74101335/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/strcpy.h"
+int main(){
+	const char *src = "f74101335";
+	char *dest = malloc(10);
+	dest = sstrcpy(dest, src);
+	printf("%s\n", dest);
+	return 0;
+}
